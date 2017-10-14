d7996335cd50797a6caef5aa7370295ae3f771b0
Andreas Blachere <dre@andreasblachere.com>
2017-10-13 20:47:37 -0700

    add mysql install script

--- config/wp-mysql-install.sql
+++ config/wp-mysql-install.sql
@@ -1,8 +1,8 @@
-create database theeverafter;
+create database [dbname];
 flush privileges;
 use mysql;
-insert into user (Host, User, Password) VALUES ('localhost', 'wp_user', PASSWORD('devpass'));
+insert into user (Host, User, Password) VALUES ('%', '[db_user]', PASSWORD('[db_pwd]'));
 flush privileges;
-grant all privileges on theeverafter.* to wp_user@localhost;
+grant all privileges on [dbname].* to [db_user]@'%';
 flush privileges;
 exit
